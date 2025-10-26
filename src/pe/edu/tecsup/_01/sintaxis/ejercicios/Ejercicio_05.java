95eba267c8743090068afd05df0587d3c1b69b56
Developer <developer@Developers-MacBook-Pro.local>
2025-10-26 12:36:07 -0500

    update

---
+++ src/pe/edu/tecsup/_01/sintaxis/ejercicios/Ejercicio_05.java
@@ -0,0 +1,25 @@
+package pe.edu.tecsup._01.sintaxis.ejercicios;
+
+public class Ejercicio_05 {
+
+    /**
+     *
+     * Ejercicio 5: Dado un numero, X = 21 , Realizar las siguiente operaciones con operadores unitarios
+     *
+     *     - Incrementar en 10 el numero
+     *     - Obtener el resto de dividir el numero entre 7
+     *     - Decrementar 4 al resultado anterior
+     *     - Dividir entre 3 al numero anterior
+     *     - Imprimir el resultado anterior con 2 decimales
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+}
