2562ffd51e4a1bcbb100f5ed87d1c4d6e5f35ccd
Developer <developer@Developers-MacBook-Pro.local>
2025-10-26 12:45:08 -0500

    update

--- src/pe/edu/tecsup/_01/sintaxis/ejercicios/Ejercicio_05.java
+++ src/pe/edu/tecsup/_01/sintaxis/ejercicios/Ejercicio_05.java
@@ -8,15 +8,26 @@ public class Ejercicio_05 {
      *
      *     - Incrementar en 10 el número
      *     - Obtener el resto de dividir el numero entre 7
-     *     - Decrementar 4 al resultado anterior
-     *     - Dividir entre 3 al numero anterior
+     *     - Decrementar en 4 al resultado anterior
+     *     - Dividir entre 3 al resultado anterior
      *     - Imprimir el resultado anterior con 2 decimales
      *
      * @param args
      */
     public static void main(String[] args) {
 
+        double x = 21;
 
+        x+=10;
+
+        x = x % 7;
+
+        x-=4;
+
+        double dividir = x/3;
+
+        System.out.println(x);
+        System.out.println(dividir);
 
 
     }
