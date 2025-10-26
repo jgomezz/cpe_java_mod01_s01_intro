9b9c318af1e6821d94cd9ab65498f491c6a6c266
Developer <developer@Developers-MacBook-Pro.local>
2025-10-26 11:43:24 -0500

    update

---
+++ src/pe/edu/tecsup/_01/sintaxis/ejercicios/Ejercicio_02.java
@@ -0,0 +1,16 @@
+package pe.edu.tecsup._01.sintaxis.ejercicios;
+
+public class Ejercicio_02 {
+
+    /**
+     *
+     * **Ejercicio 3:** Crea un programa que demuestre la pérdida de precisión al convertir de double a int..
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+
+}
