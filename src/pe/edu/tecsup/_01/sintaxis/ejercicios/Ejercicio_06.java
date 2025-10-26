a52fa8523e789c77e286034b79480f9dba3408fe
Developer <developer@Developers-MacBook-Pro.local>
2025-10-26 12:54:12 -0500

    update

---
+++ src/pe/edu/tecsup/_01/sintaxis/ejercicios/Ejercicio_06.java
@@ -0,0 +1,21 @@
+package pe.edu.tecsup._01.sintaxis.ejercicios;
+
+public class Ejercicio_06 {
+
+    /**
+     *
+     * Ejercicio 6: Crear un logica booleana
+     *
+     *     - Evaluar que una persona sea mayor edad en Perú
+     *     - Tiene documento de identidad
+     *     - Esta caminando por la calle tarde
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
