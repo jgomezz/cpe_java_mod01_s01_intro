eca41ec5e4f4ac41373e748b4a91b309a564fe8c
Developer <developer@Developers-MacBook-Pro.local>
2025-10-26 12:25:08 -0500

    update

---
+++ src/pe/edu/tecsup/_01/sintaxis/ejercicios/Ejercicio_04.java
@@ -0,0 +1,21 @@
+package pe.edu.tecsup._01.sintaxis.ejercicios;
+
+public class Ejercicio_04 {
+
+    /**
+     *
+     * Ejercicio 4: Calcular la soluci-on de una ecuacion cuadratica
+     *               2
+     *     f(x) =  ax + bx + c
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+}
