dc21d10a6ba8dcda27c1d56583e455c5dd26009a
Developer <developer@Developers-MacBook-Pro.local>
2025-11-02 08:45:56 -0500

    update

---
+++ src/pe/edu/tecsup/_01/sintaxis/ejercicios/Ejercicio_09_if.java
@@ -0,0 +1,19 @@
+package pe.edu.tecsup._01.sintaxis.ejercicios;
+
+public class Ejercicio_09_if {
+
+    /**
+     *
+     - Infants : es un programa dinámico para niños de 4 y 5 años. Accede al inglés con el BRITÁNICO. Pre Kids guía a niños en inglés.
+     - Pre Kids : 6 a 7 años
+     - Kids : Curso de inglés para niños de 8 a 10 años con actividades divertidas.
+     - Yunior : 11 a 13 años Taller presencial para niños y adolescentes, enfocado en mejorar la expresión oral.
+     - Mayores : Cursos de inglés 14 años a más Aprende inglés con el BRITÁNICO y su método Active Learning.
+     * @param args
+     */
+    public static void main(String[] args) {
+
+
+    }
+
+}
