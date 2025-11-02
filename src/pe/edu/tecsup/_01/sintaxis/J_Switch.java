925930151335351e5fc2c7279e78306dbea21c75
Developer <developer@Developers-MacBook-Pro.local>
2025-11-02 09:12:32 -0500

    update

--- src/pe/edu/tecsup/_01/sintaxis/J_Switch.java
+++ src/pe/edu/tecsup/_01/sintaxis/J_Switch.java
@@ -53,6 +53,34 @@ public class J_Switch {
 		}
 		*/
 
-	}
+
+        /*
+        int diaSemana = 1; // 1 - 7 , 8 es absurdo
+        String resultado = " SIN INFO";
+        switch(diaSemana){
+            case 1 : {
+                System.out.println("Lunes");
+                resultado = "Lunes";
+                break;
+            }
+            case 2 : System.out.println("Martes"); break;
+            case 3 :
+            case 4 :
+            case 5, 6, 7: System.out.println("Dia no registrado"); break;
+            default: System.out.println("Dia de semana no soportado");
+        }
+        System.out.println(resultado);
+        */
+
+        int diaSemana = 15; // 1 - 7 , 8 es absurdo
+
+        String resultado = switch (diaSemana) {
+            case 1 -> "Lunes";
+            case 2 -> "Martes";
+            case 3,4,5,6,7 -> "Dia no registrado";
+            default -> "Dia de semana no soportado";
+        };
+
+    }
 
 }
