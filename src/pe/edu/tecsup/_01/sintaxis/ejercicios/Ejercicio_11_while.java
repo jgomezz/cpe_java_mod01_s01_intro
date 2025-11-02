e5395fdf7f2727893da7fbbb6d94a6eb4ed21007
Developer <developer@Developers-MacBook-Pro.local>
2025-11-02 09:31:46 -0500

    update

--- src/pe/edu/tecsup/_01/sintaxis/ejercicios/Ejercicio_11_while.java
+++ src/pe/edu/tecsup/_01/sintaxis/ejercicios/Ejercicio_11_while.java
@@ -5,13 +5,24 @@ public class Ejercicio_11_while {
     /**
      *
      *  Imprimir los número enteros entre 20 y 40
+     *  inclusive el 20 y el 40
+     *
+     *  Exception :
+     *   Si hubiese un numero primo, la impresion se detiene
+     *   23,29,31,37
+     *
+     *  NOTA : Para salir de un bucle se usa la sentencia break;
      *
      * @param args
      */
     public static void main(String[] args) {
 
+        int numero = 20; // empezamos justo después de 20
 
-
+        while (numero <= 40) { // hasta justo antes de 40
+            System.out.println(numero);
+            numero++; // aumentar en 1
+        }
     }
 
 }
